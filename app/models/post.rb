36f9822b9a107031d4f92fd1ff602d02fb3724d9
donellgill <donell.gill@outlook.com>
2021-09-05 09:10:36 -0400

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+
+    validate :title_is_clickbaity?
+
+    clickbait = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+    ]
+
+    def title_is_clickbaity?
+        if clickbait.none? { |pat| pat.match title }
+            errors.add(:title, "Must be clickbait.")
+        end
+    end
 end
